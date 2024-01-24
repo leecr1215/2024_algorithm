709fbba01196575fcf69a0dfb009beb5e65aab51
chelim Lee <leecr1215@naver.com>
2024-01-24 12:55:03 +0900

    Time: 55 ms (38.62%), Space: 49.1 MB (5.01%) - LeetHub

--- LeetCode/Easy/2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
+++ LeetCode/Easy/2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -6,15 +6,9 @@ var fibGenerator = function*() {
     let n_1 = 1;
     
     for(let i = 0; i <= 50; i++){
-        if(i === 0) yield n_2;
-        if(i === 1) yield n_1;
+        yield n_2;
         
-        let n = n_2 + n_1;
-        
-        n_2 = n_1;
-        n_1 = n;
-        
-        yield n;
+        [n_2, n_1] = [n_1, n_2 + n_1];
             
     }
 };
