3e4581471d567164792fce45b64aa43fb8818ee0
chelim Lee <leecr1215@naver.com>
2024-01-29 21:47:06 +0900

    [Silver IV] Title: 제로, Time: 240 ms, Memory: 23164 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int k = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < k; i++){
+            int num = Integer.parseInt(br.readLine());
+
+            if(num == 0){
+                stack.pop();
+                continue;
+            }
+
+            stack.push(num);
+        }
+
+        int sum = 0;
+
+        for(int s : stack){
+            sum += s;
+        }
+
+        System.out.println(sum);
+
+    }
+}
