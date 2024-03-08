ff23874d70b2a9e892cb92985f066a105195ae04
chelim Lee <leecr1215@naver.com>
2024-03-08 09:23:38 +0900

    Time: 51 ms (91.38%), Space: 48.7 MB (75.87%) - LeetHub

--- LeetCode/Medium/2637-promise-time-limit/2637-promise-time-limit.js
+++ LeetCode/Medium/2637-promise-time-limit/2637-promise-time-limit.js
@@ -5,24 +5,39 @@
  */
 // fn에서 걸리는 시간이 t초 안 쪽일 때는 ok
 // 그 이상이면  time exceeded
+// 처음 코드
+// var timeLimit = function(fn, t) {
+//     const ERROR_MESSAGE = "Time Limit Exceeded";
+    
+//     return async function(...args) {
+
+//         return new Promise(async(resolve, reject)=>{
+//             try{
+//                 setTimeout(() => {
+//                 reject(ERROR_MESSAGE);
+//                 }, t);
+            
+//                 resolve(await fn(...args));
+//             }
+//             catch(err){
+//                 reject(err);
+//             }
+               
+//         });
+//     }
+// };
+
 var timeLimit = function(fn, t) {
     const ERROR_MESSAGE = "Time Limit Exceeded";
     
     return async function(...args) {
-
-        return new Promise(async(resolve, reject)=>{
-            try{
-                setTimeout(() => {
+        const promise1 = fn(...args);
+        const promise2 = new Promise((resolve, reject)=>{
+            setTimeout(()=>{
                 reject(ERROR_MESSAGE);
-                }, t);
-            
-                resolve(await fn(...args));
-            }
-            catch(err){
-                reject(err);
-            }
-               
+            }, t);
         });
+        return Promise.race([promise1, promise2]);
     }
 };
 
