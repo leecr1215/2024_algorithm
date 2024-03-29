27f08288d4e12c12b080bb261104698f4c796709
chelim Lee <leecr1215@naver.com>
2024-03-29 16:39:42 +0900

    [Silver III] Title: N과 M (7), Time: 660 ms, Memory: 220536 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static int[] arr;
+    static int[] picked;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        picked = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        duplicatePermutation(0);
+
+        System.out.println(sb.toString());
+    }
+
+    public static void duplicatePermutation(int cnt){
+        if(cnt == M){
+            for(int num : picked){
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < N; i++){
+            picked[cnt] = arr[i];
+            duplicatePermutation(cnt + 1);
+        }
+    }
+}
