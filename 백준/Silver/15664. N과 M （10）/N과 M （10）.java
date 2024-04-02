d2d0cd1269b67111ada1886c615a14b5997e4434
chelim Lee <leecr1215@naver.com>
2024-04-02 17:52:01 +0900

    [Silver II] Title: N과 M (10), Time: 80 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static int[] arr;
+    static int[] picked;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        picked = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        combination(0, 0);
+
+        for(String str : set){
+            System.out.println(str);
+        }
+
+    }
+    static Set<String> set = new LinkedHashSet<>();
+    public static void combination(int start, int cnt){
+        if(cnt == M){
+            for(int num : picked){
+                sb.append(num).append(" ");
+            }
+            set.add(sb.toString());
+            sb.setLength(0);
+            return;
+        }
+
+        for(int i = start; i < N; i++){
+            picked[cnt] = arr[i];
+            combination(i + 1, cnt + 1);
+        }
+    }
+}
