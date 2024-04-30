66623a2635ab968fd439c6a4610ece5926ff5ec5
chelim Lee <leecr1215@naver.com>
2024-04-30 14:28:55 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 432 ms, Memory: 292744 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -18,26 +18,19 @@ public class Main {
         int i = 0;
         int[] newArr = new int[n];
 
-        while(list.size() >= k){
+        while(!list.isEmpty()){
             // k-1개까지 뽑아서 뒤로 넣기
             // k번째 데이터는 remove
 
-            for(int j = 0; j < k-1; j++){
+            int size = list.size() >= k ? 0 : list.size();
+
+            for(int j = 0; j < k-1-size; j++){
                 list.add(list.removeFirst());
             }
 
             newArr[i++] = list.removeFirst();
         }
 
-        while(!list.isEmpty()){
-            int size = list.size();
-
-            for(int j = 0; j < k-size-1; j++){
-                list.add(list.removeFirst());
-            }
-
-            newArr[i++] = list.removeFirst();
-        }
 
         System.out.println(Arrays.toString(newArr).replace("[", "<").replace("]", ">"));
     }
