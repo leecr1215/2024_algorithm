b2aeee3fffff4572e73b35f5ef6446d5fba09e87
chelim Lee <leecr1215@naver.com>
2024-07-30 21:13:07 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,13 +1,12 @@
 function solution(n) {
     let answer = 0;
-    
-    const binaryN = n.toString(2);
-    const cnt1 = binaryN.split('1').length - 1;
+
+    const cnt = n.toString(2).split('1').length - 1;
     
     
     while(true){
         n++;
-        if(n.toString(2).split('1').length - 1 === cnt1){
+        if(n.toString(2).split('1').length - 1 === cnt){
             answer = n;
             break;
         }
