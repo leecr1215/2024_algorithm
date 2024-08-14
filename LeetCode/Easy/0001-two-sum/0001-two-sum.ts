54f04c9216a545c810bc7a9e4025f7a54ec490ec
chelim Lee <leecr1215@naver.com>
2024-08-14 21:42:27 +0900

    Time: 168 ms (11.53%), Space: 51.8 MB (93.19%) - LeetHub

---
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number): number[] {
+    
+    for(let i = 0; i < nums.length-1; i++){
+        for(let j = 0; j < nums.length; j++){
+            if(i !== j){
+                if(nums[i]+nums[j] === target){
+                    return [i, j];
+                }
+            }
+        }
+    }
+};
