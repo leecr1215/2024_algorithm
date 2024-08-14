3984e7e8b8fbabb4f19d292cb9166025c606f3e4
chelim Lee <leecr1215@naver.com>
2024-08-14 21:56:25 +0900

    Time: 60 ms (81.50%), Space: 53.4 MB (10.56%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.ts
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.ts
@@ -1,12 +1,14 @@
 function twoSum(nums: number[], target: number): number[] {
+    // key : num, value : index
+    const map = new Map();
     
     for(let i = 0; i < nums.length; i++){
         const num = target - nums[i];
-        const index = nums.indexOf(num);
         
-        if(index !== -1 && index !== i){
-            // 배열에 있으면서 자기 자신이 아닐 때
-            return [i, index];
+        if(map.get(num)!==undefined){
+            return [i, map.get(num)]
         }
+        
+        map.set(nums[i], i);
     }
 };
