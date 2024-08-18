d65441e5490b42ab60a79fd0c4937166a32c8e8e
chelim Lee <leecr1215@naver.com>
2024-08-18 15:21:34 +0900

    Time: 158 ms (14.60%), Space: 59.3 MB (37.89%) - LeetHub

--- LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.ts
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.ts
@@ -1,12 +1,3 @@
 function isPalindrome(x: number): boolean {
-    const str = String(x);
-    
-    const checkIdx:number = parseInt(str.length / 2 + '');
-    
-    for(let i = 0; i < checkIdx; i++){
-        if(str[i] !== str[str.length-1-i]) return false;
-    
-    }
-    
-    return true;
+   return x.toString().split('').reverse().join('') === x.toString() ? true : false;
 };
