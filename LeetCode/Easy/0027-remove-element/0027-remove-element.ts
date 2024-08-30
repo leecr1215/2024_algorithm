017ad4919b52eee492c408b70c6578a34349efbc
chelim Lee <leecr1215@naver.com>
2024-08-30 18:31:14 +0900

    Time: 61 ms (42.51%), Space: 51.4 MB (57.91%) - LeetHub

---
+++ LeetCode/Easy/0027-remove-element/0027-remove-element.ts
@@ -0,0 +1,11 @@
+function removeElement(nums: number[], val: number): number {
+    let cnt = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] !== val){
+            nums[cnt++] = nums[i];
+        }
+    }
+    
+    
+    return cnt;
+};
