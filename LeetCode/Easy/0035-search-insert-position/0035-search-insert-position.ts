964ebde54bc09522884ae9a21567fcf789a41964
chelim Lee <leecr1215@naver.com>
2024-09-04 21:23:46 +0900

    Time: 66 ms (14.26%), Space: 51.3 MB (77.15%) - LeetHub

---
+++ LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.ts
@@ -0,0 +1,23 @@
+function searchInsert(nums: number[], target: number): number {
+   return binarySearch(nums, target);
+};
+
+function binarySearch(nums: number[], target: number):number{
+        let left = 0;
+        let right = nums.length - 1;
+        let mid = parseInt((left + right) / 2 +'');
+        
+        while(left <= right){
+            if(nums[mid] < target){
+                left = mid + 1;
+            }else if(nums[mid] > target){
+                right = mid - 1;
+            }else{
+                return mid;
+            }
+            
+            mid = parseInt((left + right) / 2+'');
+        }
+    
+        return left;
+}
