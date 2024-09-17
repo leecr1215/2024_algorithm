d9b9f27e871166d8f3b139fb876a2e4c7eaf5b19
chelim Lee <leecr1215@naver.com>
2024-09-17 17:36:09 +0900

    Time: 62 ms (21.25%), Space: 50.3 MB (25.17%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.ts
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.ts
@@ -2,7 +2,7 @@ function climbStairs(n: number): number {
     if(n == 1) return 1;
     if(n == 2) return 2;
     
-    let dp = [0, 1, 2];
+    let dp = [1, 1, 2];
     
     for(let i = 3; i <= n; i++){
         dp.push(dp[i-1] + dp[i-2]);
