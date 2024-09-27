fe1c56d6221ec69d004439507c899cd013fc79e5
chelim Lee <leecr1215@naver.com>
2024-09-27 23:10:10 +0900

    Time: 1023 ms (7.95%), Space: 0B (100.00%) - LeetHub

---
+++ LeetCode/Easy/0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,5 @@
+SELECT w.id
+FROM Weather w
+WHERE w.temperature > (SELECT temperature
+                       FROM Weather
+                       WHERE recordDate = DATE_SUB(w.recordDate, INTERVAL 1 DAY));
