f1b64914060b8920a6326331ea2cafa21e08f091
chelim Lee <leecr1215@naver.com>
2024-12-17 15:58:54 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,17 @@
+/**
+1 : 1
+2 : 2
+3 : 3개
+4 : 5개
+**/
+function solution(n) {
+    let dp = new Array(n+1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(let i = 3; i <= n; i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    }
+    
+    return dp[n];
+}
