5ecb4fe2a80051248a3042ec7337d50ba68d9891
chelim Lee <leecr1215@naver.com>
2025-01-13 15:15:47 +0900

    Time: 616 ms (58.86%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/Medium/1393-capital-gainloss/1393-capital-gainloss.sql
@@ -0,0 +1,7 @@
+# 2025.01.13
+SELECT stock_name, SUM(CASE 
+                        WHEN operation="Buy" THEN -price
+                        ELSE price
+                        END) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name;
