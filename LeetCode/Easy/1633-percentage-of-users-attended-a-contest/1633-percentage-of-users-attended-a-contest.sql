ff408d30169c2f15851ff213344781676ff65643
chelim Lee <leecr1215@naver.com>
2025-02-03 23:26:52 +0900

    Time: 1821 ms (39.26%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/Easy/1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,5 @@
+# 2025.02.03
+SELECT contest_id, ROUND((COUNT(user_id) / (SELECT COUNT(*) FROM Users)) * 100, 2) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id;
