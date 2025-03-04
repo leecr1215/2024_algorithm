5bc5151e441841637c57a730e001a6e1c4236bba
chelim Lee <leecr1215@naver.com>
2025-03-05 00:29:11 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,12 +1,3 @@
-/**
-몫 3 나머지 4 -> (총 4개의 줄, 제일 윗 줄은 오른쪽 4개 채워진 상태) =
-
-8의 경우, 8 / 6 -> 몫 1 나머지 2 (둘쨋줄의 오른쪽에서 2칸) => 둘째줄
-
-13 / 3 은 몫4 나머지 1
-6 / 3은 몫 2 나머지 0
-
-**/
 function solution(n, w, num) {
     
     if(n <= w) return 1;
@@ -39,11 +30,6 @@ function solution(n, w, num) {
         answer++;
     }
     
-    console.log(`(${x1}, ${y1}), (${x2}, ${y2})`);
-    
-    return answer;
-}
 
-function isSameDirection(quotient1, quotient2){
-    return (quotient1 % 2 == 0 && quotient2 % 2 == 0) || (quotient1 % 2 != 0 && quotient2 % 2 != 0);
+    return answer;
 }
