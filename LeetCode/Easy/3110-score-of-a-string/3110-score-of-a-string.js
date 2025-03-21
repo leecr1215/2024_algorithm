3f6d614718808cfd93d9fede924f37438c002e7f
chelim Lee <leecr1215@naver.com>
2025-03-21 21:53:31 +0900

    Time: 0 ms (100%), Space: 53.8 MB (44.95%) - LeetHub

---
+++ LeetCode/Easy/3110-score-of-a-string/3110-score-of-a-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function(s) {
+    let answer = 0;
+
+    for(let i = 0; i < s.length-1; i++){
+        answer += Math.abs(s[i].charCodeAt() - s[i+1].charCodeAt());
+    }
+
+    return answer;
+};
