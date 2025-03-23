f483bea8d826f42cb9be605237633d1c61f24c8b
chelim Lee <leecr1215@naver.com>
2025-03-23 23:42:08 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,8 @@
+-- 2025.03.23
+SELECT info.ITEM_ID, info.ITEM_NAME, info.RARITY
+FROM ITEM_INFO info INNER JOIN ITEM_TREE tree
+ON info.ITEM_ID = tree.ITEM_ID
+WHERE tree.PARENT_ITEM_ID IN (SELECT ITEM_ID
+                        FROM ITEM_INFO
+                        WHERE RARITY = 'RARE')
+ORDER BY info.ITEM_ID DESC;
